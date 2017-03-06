0df6ee4500f1a1a1470d607212e282aa4d06dbfa
Kellyanne Stack <kstack88@uw.edu>
2017-03-06 01:03:20 -0800

    Add text input widgets: :q

--- server.R
+++ server.R
@@ -1,5 +1,18 @@
 library(shiny)
-
+library(httr)
+library(jsonlite)
+api.key <- "89b7ffa4912ce0420e1611ed804a106b"
 server <- function(input, output) {
+  artist.data <- reactive({
+    base.uri <- "http://api.musixmatch.com/ws/1.1/"
+    endpoint <- "artist.get"
+    query.params <- list(artist_id=input$artist)
+    
+  })
+  output$artist.test <- renderText({
+    input$artist
+  })
   
 }
+
+shinyServer(server)
--- ui.R
+++ ui.R
@@ -1,27 +1,28 @@
 library(shiny)
-source('apikey.R')
+#source('apikey.R')
 ui <- navbarPage("Navbar",
-                 tabPanel("Artist",
-                          sidebarLayout(
-                            sidebarPanel(
-                              textInput("text", label = "Text Input", value = "Enter lyric here")
+          tabPanel("Artist",
+                 sidebarLayout(
+                       sidebarPanel(
+                            textInput("artist", label = "Artist Input", value = "Enter artist"),
+                            textInput("lyric", label = "Lyric Input", value = "Enter lyric here")
+                            
                             ),
-                            mainPanel(
-                              #plotOutput("plot")
-                              "he"
-                            )
-                          )
-                          
-                 ),
-                 tabPanel("Genre",
-                          sidebarLayout(
-                            sidebarPanel(
-                              "he"
-                            ),
-                            mainPanel(
+                       mainPanel(
+                          #plotOutput("plot")
+                             verbatimTextOutput("artist.test")
+                       )
+                  )
+          ),
+          tabPanel("Genre",
+                sidebarLayout(
+                      sidebarPanel(
+                            "he"
+                      ),
+                      mainPanel(
                              "he" 
-                            )
-                          )
-                          )
+                      )
+                )
+         )
 )
-
+shinyUI(ui)
