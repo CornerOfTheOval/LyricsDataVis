b508be3a3f751c5cf7d6babf35be734ba3cf6553
Kellyanne Stack <kstack88@uw.edu>
2017-03-07 21:42:02 -0800

    Turn artist ids into vector

--- server.R
+++ server.R
@@ -5,10 +5,10 @@ source('apikey.R')
 
 server <- function(input, output) {
   artist.data <- reactive({
-    #q_artist = "Beyonce" 
+    q_artist = "Beyonce" # q_artist = input$artist
     base.uri <- "http://api.musixmatch.com/ws/1.1/"
     endpoint <- "artist.search" # at top of ereactive use input$tab.type, ifstatement, then choose enpoint from API based on that
-    query.params <- list(q_artist="beyonce", apikey = api.key, page_size=100) 
+    query.params <- list(q_artist="arctic monkeys", apikey = api.key, page_size=100) 
     uri <- paste0(base.uri, endpoint)
     response <- GET(uri, query = query.params)
     body <- content(response, "text")
@@ -16,13 +16,11 @@ server <- function(input, output) {
     
     # Un-nests the lists (similar to flatten) to get data frame of interest
     artist.search <- parsed.data$message$body$artist_list$artist
-    View(artist.search)
-    View(flatten(parsed.data$message$body$artist_list))
-    names(artist.search)
-    View(artist.search)
     artist.ids <- artist.search %>% 
       select(artist_id)
-artist.ids
+    
+    # stores ids as vector to be passed into a for loop
+    artist.ids <- as.vector(artist.ids$artist_id)
     #filter(row_number() == 1) # just saves exact name, does not include collabs, "feat"
     # instead of filtering, save df as is (just ids) as vector
     # then, as a for loop, go through and get albums for every single artist listed
@@ -30,7 +28,7 @@ artist.ids
     # use albums ids to get track
     artist.ids # all artist ids involving the artist, including collabs/feat.
     
-    listy.try <- c(18927, 24456502)
+    # empty data frame album.list to add all relevent album ids for the artist
     album.list <- c()
     for(id in artist.ids) {
       
@@ -44,17 +42,20 @@ artist.ids
     album.body <- content(album.response, "text")
     album.data <- fromJSON(album.body)
     album.data <- album.data$message$body$album_list$album
-    if(!(is.null(album.data))) {
+    
+    # if the data frame is not null, then the for loop continues to gather
+    # the album id, if not, it moves on to next element in vector (the next artist id)
+     if(!(is.null(album.data))) {
     # makes all album names same case
       album.data$album_name <- tolower(album.data$album_name)
 
-    # gets unique albums
+    # filters for unique album names (some are repeated)
       album.data <- album.data[!duplicated(album.data$album_name),]
     
     # saves all album ids for the artist
       album.id <- album.data$album_id
       album.list <- append(album.list, album.id)
-    }
+     }
     }
     album.list
     # create empty data frame for all songs to be added to
