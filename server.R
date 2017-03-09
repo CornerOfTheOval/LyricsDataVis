91cfe486cefe450bab007c566fdfb4d7ad2d2e19
Kellyanne Stack <kstack88@uw.edu>
2017-03-08 19:22:43 -0800

    Add plotly function

--- server.R
+++ server.R
@@ -3,17 +3,19 @@ library(httr)
 library(jsonlite)
 source('apikey.R')
 library(ggplot2)
+library(plotly)
 
 server <- function(input, output) {
   
   artist.data <- reactive({
+    
     # Artist Search
     base.uri <- "http://api.musixmatch.com/ws/1.1/"
     endpoint <- "artist.search" 
     query.params <- list(q_artist = input$artist, apikey = api.key, page_size = 100) 
     uri <- paste0(base.uri, endpoint)
     response <- GET(uri, query = query.params)
-    body <- httr::content(response, "text")
+    body <- content(response, "text")
     parsed.data <- fromJSON(body)
     # Flattens parsed data list to get data frame of interest
     artist.search <- parsed.data$message$body$artist_list$artist
@@ -29,23 +31,21 @@ server <- function(input, output) {
       
       # Gets all albums associated with the artist id
       song.uri <- paste0(base.uri, "track.search")
-      query.params.song <- list(q_artist= name, apikey = api.key, page_size = 100, f_lyrics_language = "en") 
+      query.params.song <- list(q_artist= name, apikey = api.key, 
+                                page_size = 100, f_lyrics_language = "en") 
       song.response <- GET(song.uri, query = query.params.song)
-      song.body <- httr::content(song.response, "text")
+      song.body <- content(song.response, "text")
       song.parsed <- fromJSON(song.body)
       song.data <- song.parsed$message$body$track_list$track 
       if(!is.null(song.data)){
-      song.data <- song.data %>% 
-        select(track_name, track_id)
-      no.dupes <- song.data[!duplicated(song.data$track_name),]
-      # all songs for all artists
-      song.list <- c(song.list, no.dupes)
-
+        song.data <- song.data %>% 
+          select(track_name, track_id)
+        no.dupes <- song.data[!duplicated(song.data$track_name),]
+        # all songs for all artists
+        song.list <- c(song.list, no.dupes)
        } 
     }
     
-    # Empty vector for all songs to be appended to
-
     # Extract only unique track_ids
     songs.unique <- unique(song.list$track_id)
     
@@ -64,13 +64,13 @@ server <- function(input, output) {
       
       # Only proceed through for loop if the page exists
       if(response$status_code == 200) {
-        body <- httr::content(response, "text") 
+        body <- content(response, "text") 
         lyric.parsed <- fromJSON(body)
         lyric.body <- lyric.parsed$message$body$lyrics$lyrics_body
+        
         # gets rid of everything after "This Lyrics" commerical use warning
         lyric.body <- gsub('This\\sLyrics.*', "", lyric.body) 
         # splits entire lyric into individual words
-        # http://stackoverflow.com/questions/26159754/using-r-to-find-top-ten-words-in-a-text
         lyric.split <- strsplit(paste(lyric.body, collapse = " "), "[[:space:]]+")[[1]]
       
         # removes common stop words
@@ -78,11 +78,8 @@ server <- function(input, output) {
         
         # table sorts each word into a box with its matching word, sorts in ascending order
         # select the most frequent word 
-        #most.freq <- tail(sort(table(lyric.split)), 1) #turn 1 into input for each song
-        
-        # current --> stores one word in track.lyrics, should i just store entire lyrics
+
         # Add track ids to empty vector
-        #track.lyrics <- append(track.lyrics, most.freq)
         track.lyrics <- append(track.lyrics, lyric.split)
       }
     }
@@ -90,17 +87,20 @@ server <- function(input, output) {
       filter(!track.lyrics == "...") %>% 
       arrange(desc(Freq)) %>% 
       head(input$num.words)
-
-  
     return(word.df)
     
   })
-  
+  # color based on if word is more than 3 letters
+  # hover show both values
   output$artist.plot <- renderPlot({
     plot <- ggplot(data = artist.data(), mapping = aes(x = reorder(track.lyrics, -Freq), y = Freq)) +
       geom_bar(stat = 'identity') +
-      labs(x = "Top words", y = "Frequency", title = paste(input$artist, "Top Lyrics")) 
+      geom_point() +
+      labs(x = paste("Top", input$num.words, "words"), y = "Frequency", title = paste(input$artist, "Top Lyrics"))  
+    plot <- ggplotly(plot)  
     return(plot)
   })
   
+
 }
+shinyServer(server)
--- ui.R
+++ ui.R
@@ -4,8 +4,6 @@ ui <- navbarPage("Navbar",
                           sidebarLayout(
                             sidebarPanel(
                               textInput("artist", label = "Artist Input", placeholder = "Enter artist"),
-                              # add slider input for user to choose how many top songs to include
-                              # limit --> 50?
                               sliderInput("num.words", label = "Choose the amount of top words", min = 1, max = 50,
                                           step = 1, value = 10)
 
@@ -14,17 +12,8 @@ ui <- navbarPage("Navbar",
                               plotOutput("artist.plot")
                             )
                           )
-                 ),
-                 tabPanel("Genre",
-                          sidebarLayout(
-                            sidebarPanel(
-                              "he"
-                            ),
-                            mainPanel(
-                              "he" 
-                            )
-                          )
                  )
+   
 )
 
 shinyUI(ui)
